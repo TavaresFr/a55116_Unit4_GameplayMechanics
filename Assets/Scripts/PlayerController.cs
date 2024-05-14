cb025e0b7579ecafef5a8fc44f1c4e1a35a46175
TavaresFr <152510118+TavaresFr@users.noreply.github.com>
2024-05-14 22:36:06 +0100

    Lesson 4.3 - PowerUp and CountDown

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,14 @@ public class PlayerController : MonoBehaviour
 
     GameObject focalPoint;
 
+    [SerializeField] GameObject powerUpIndicator;
+
     Rigidbody rb;
+
+    bool hasPowerup;
+
+    float powerUpStrength = 15f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -19,5 +26,38 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxisRaw("Vertical");
         rb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        powerUpIndicator.transform.position = transform.position + new Vector3 (0, -0.5f, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerUpIndicator.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerUpCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerUpIndicator.SetActive(false);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Hit with Powerup baby");
+            enemyRb.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            hasPowerup = false;
+            powerUpIndicator.SetActive(false);
+        }
     }
 }
